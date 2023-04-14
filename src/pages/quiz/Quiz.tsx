9a0386c772c48e597ac6b865b4c300217a5fde98
Lukasnvc <lukasnvc@gmail.com>
2023-04-14 15:17:21 +0300

    implimented countdown and summary

--- src/pages/quiz/Quiz.tsx
+++ src/pages/quiz/Quiz.tsx
@@ -1,10 +1,13 @@
-import React, { useContext, useState } from "react";
-import { blackColor, greyColor, mainShadow } from "../../consts/colors";
+import { greyColor, violetDark, violetLight } from "../../consts/colors";
+import { useContext, useState } from "react";
 
 import Button from "../../components/Button";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import EndCard from "../../components/EndCard";
 import { HOME_PATH } from "../../routes/const";
 import QuestionCard from "../../components/QuestionCard";
 import { UserContext } from "../../context/UserContext";
+import { VscDebugRestart } from "react-icons/vsc";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { useQuestions } from "../../hooks/useQuestions";
@@ -19,6 +22,7 @@ export type AnswerObject = {
 };
 
 const Quiz = () => {
+  const [key, setKey] = useState(0);
   const [number, setNumber] = useState(0);
   const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
@@ -48,13 +52,34 @@ const Quiz = () => {
       setGameOver(true);
     } else {
       setNumber(nextQ);
+      setKey((prev) => prev + 1);
+      return { shouldRepeat: true };
     }
   };
 
   return (
     <Wrapper>
-      <p>{difficulty}</p>
-      <p>Score : {score}</p>
+      {!gameOver && (
+        <CountdownCircleTimer
+          size={140}
+          key={key}
+          onComplete={nextQuestion}
+          isPlaying={!gameOver}
+          duration={15}
+          colors={[violetLight, violetDark, "#A30000", "#A30000"]}
+          colorsTime={[7, 5, 2, 0]}>
+          {({ remainingTime }) => <Time>{remainingTime}</Time>}
+        </CountdownCircleTimer>
+      )}
+      {!gameOver ? (
+        <Score>
+          <p>DIFFICULTY : {difficulty}</p>
+          <p>Score : {score}</p>
+        </Score>
+      ) : (
+        <EndCard summary={userAnswers} score={score} />
+      )}
+
       {!isLoading ? (
         <>
           {!gameOver ? (
@@ -75,14 +100,14 @@ const Quiz = () => {
       !isLoading &&
       userAnswers.length === number + 1 &&
       number !== TOTAL_QUESTIONS ? (
-        <StyledBtn onClick={nextQuestion}>
-          NEXT <br />
-          QUESTION
-        </StyledBtn>
+        <StyledBtn onClick={nextQuestion}>NEXT</StyledBtn>
       ) : null}
       {gameOver && (
         <>
-          <Button onClick={() => navigate(HOME_PATH)}>RESTART</Button>
+          <Restart onClick={() => navigate(HOME_PATH)}>
+            Restart
+            <VscDebugRestart />
+          </Restart>
         </>
       )}
     </Wrapper>
@@ -92,22 +117,54 @@ const Quiz = () => {
 export default Quiz;
 
 const Wrapper = styled.div`
+  padding-top: 20px;
   display: flex;
   flex-direction: column;
   align-items: center;
   max-width: 1100px;
-  height: 90vh;
+  height: 100vh;
 `;
 
 const StyledBtn = styled(Button)`
-  margin: 40px;
-  width: 160px;
-  height: 160px;
   font-size: 1.2rem;
-  border-radius: 100%;
-  box-shadow: ${mainShadow};
+  align-items: center;
+  margin-top: 50px;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
   border: 3px solid ${greyColor};
-  font-family: "Mochiy Pop One", sans-serif;
+  &:hover {
+    background-color: #59bc86;
+  }
+`;
+
+const Score = styled.div`
+  font-size: 1.3rem;
+  margin: 30px;
+  color: ${violetDark};
+  text-transform: capitalize;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  p {
+    margin: 10px;
+  }
+`;
+
+const Time = styled.p`
+  color: ${violetDark};
+  font-size: 1.8rem;
+`;
+
+const Restart = styled(Button)`
+  align-items: center;
+  padding: 20px 40px;
+  font-size: 1.3rem;
+  margin-top: 50px;
+  svg {
+    margin-left: 10px;
+    font-size: 1.6rem;
+  }
   &:hover {
     background-color: #59bc86;
   }
